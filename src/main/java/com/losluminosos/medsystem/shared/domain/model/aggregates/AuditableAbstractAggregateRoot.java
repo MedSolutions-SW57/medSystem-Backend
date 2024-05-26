3cbaaa4abf178b8ad1e6089c61a461ecd38cabfa
jorgealtamirano1 <u202215888@upc.edu.pe>
2024-05-26 12:38:20 -0500

    docs: AuditableAbstractAggregateRoot and AuditableModel files generated.

---
+++ src/main/java/com/losluminosos/medsystem/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -0,0 +1,30 @@
+package com.losluminosos.medsystem.shared.domain.model.aggregates;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
+
+import java.util.Date;
+
+public abstract class AuditableAbstractAggregateRoot<T extends AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date createdAt;
+
+    @Getter
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updatedAt;
+}
