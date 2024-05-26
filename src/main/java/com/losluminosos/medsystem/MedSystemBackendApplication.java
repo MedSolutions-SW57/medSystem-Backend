bf3356eea6a8789fd8669fd0e963afe58de968a6
jorgealtamirano1 <u202215888@upc.edu.pe>
2024-05-25 21:52:00 -0500

    chore: initial commit.

---
+++ src/main/java/com/losluminosos/medsystem/MedSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.losluminosos.medsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/losluminosos/medsystem/MedSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losluminosos.medsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
