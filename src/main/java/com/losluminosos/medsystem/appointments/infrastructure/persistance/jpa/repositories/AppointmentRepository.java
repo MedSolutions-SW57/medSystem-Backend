89230cd3eeaaf3c6a6ae79786fff021e1e2732ee
jorgealtamirano1 <u202215888@upc.edu.pe>
2024-06-09 17:54:42 -0500

    chore: removed unnecessary files

--- src/main/java/com/losluminosos/medsystem/appointments/infrastructure/persistance/jpa/repositories/AppointmentRepository.java
+++ src/main/java/com/losluminosos/medsystem/appointments/infrastructure/persistance/jpa/repositories/AppointmentRepository.java
@@ -7,10 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-    // Buscar una cita por fecha y hora específica
     Optional<Appointment> findByAppointmentTimePoint(AppointmentTimePoint appointmentTimePoint);
-
-    // Verificar si existe una cita en una fecha y hora específica
     boolean existsByAppointmentTimePoint(AppointmentTimePoint appointmentTimePoint);
 
 }
