e38a552f3bacf37e18fda7f32c5ae86d242622f9
CarlosG345 <u202210364@upc.edu.pe>
2024-06-09 18:18:26 -0500

    feat: deleted unnecessary files

--- src/main/java/com/losluminosos/medsystem/medicalservice/interfaces/rest/resources/HistoryResource.java
+++
@@ -1,4 +0,0 @@
-package com.losluminosos.medsystem.medicalservice.interfaces.rest.resources;
-
-public record HistoryResource(Long id, String reason, String date,  String patientId, String doctorId) {
-}
