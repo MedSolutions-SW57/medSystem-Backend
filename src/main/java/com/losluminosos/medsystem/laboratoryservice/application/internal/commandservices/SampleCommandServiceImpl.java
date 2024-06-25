c3efeb2cf633f8c2a2edfbbe534a56bc3c085c48
JuanMontes18 <u202210775@upc.edu.pe>
2024-06-24 19:35:35 -0500

    feat: added the command service and its sample implementation

--- src/main/java/com/losluminosos/medsystem/laboratoryservice/application/internal/commandservices/SampleCommandServiceImpl.java
+++ src/main/java/com/losluminosos/medsystem/laboratoryservice/application/internal/commandservices/SampleCommandServiceImpl.java
@@ -2,6 +2,7 @@ package com.losluminosos.medsystem.laboratoryservice.application.internal.comman
 
 import com.losluminosos.medsystem.laboratoryservice.domain.model.aggregates.Sample;
 import com.losluminosos.medsystem.laboratoryservice.domain.model.commands.CreateSampleCommand;
+import com.losluminosos.medsystem.laboratoryservice.domain.model.commands.DeleteSampleCommand;
 import com.losluminosos.medsystem.laboratoryservice.domain.services.SampleCommandService;
 import com.losluminosos.medsystem.laboratoryservice.infrastructure.persistence.jpa.repositories.SampleRepository;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,11 @@ public class SampleCommandServiceImpl implements SampleCommandService {
         var sample = new Sample(command);
         var createdSample = sampleRepository.save(sample);
         return Optional.of(createdSample);
+    }
+
+    @Override
+    public void handle(DeleteSampleCommand command) {
+        var sample = sampleRepository.findSampleByCode(command.code());
+        sample.ifPresent(sampleRepository::delete);
     }
 }
